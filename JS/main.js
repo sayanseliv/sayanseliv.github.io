4fbc498ed809e709cc7c67cfbdabf60df11adca9
sayanseliv <sayanseliv@gmail.com>
2021-11-23 23:10:35 +0200

    changes

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,15 @@
 const left = document.querySelector(".wrapper_l_col");
 const right = document.querySelector(".wrapper_r_col");
-const age = document.querySelector(".age")
-
-age.innerHTML = Math.floor((new Date() - new Date(1985,03,27)) / 1000 / 60 / 60 / 24 / 365.25);
+const age = document.querySelector(".age");
+const trans = "transparent";
+const white = "white";
+age.innerHTML =
+  ((new Date().getTime() - new Date(1985, 03, 27)) /
+    (24 * 3600 * 365.25 * 1000)) |
+  0;
 changeAnimation(left, right);
 
 function changeAnimation(a, b) {
-  
   if (!a || !b) {
     return;
   }
@@ -25,27 +28,27 @@ function changeAnimation(a, b) {
 }
 
 function animationLeft(el) {
-  if (window.innerWidth < 768) return
+  if (window.innerWidth < 768) return;
   el.animate(
     [
       // keyframes
       {
-        borderTopColor: "white",
-        borderRightColor: "transparent",
-        borderBottomColor: "transparent",
-        borderLeftColor: "transparent",
+        borderTopColor: white,
+        borderRightColor: trans,
+        borderBottomColor: trans,
+        borderLeftColor: trans,
       },
       {
-        borderTopColor: "transparent",
-        borderLeftColor: "white",
-        borderBottomColor: "transparent",
-        borderRightColor: "transparent",
+        borderTopColor: trans,
+        borderLeftColor: white,
+        borderBottomColor: trans,
+        borderRightColor: trans,
       },
       {
-        borderTopColor: "transparent",
-        borderRightColor: "transparent",
-        borderBottomColor: "white",
-        borderLeftColor: "transparent",
+        borderTopColor: trans,
+        borderRightColor: trans,
+        borderBottomColor: white,
+        borderLeftColor: trans,
       },
     ],
     {
@@ -58,34 +61,45 @@ function animationLeft(el) {
 }
 
 function animationRight(el) {
-  if (window.innerWidth < 768) return
+  if (window.innerWidth < 768) return;
   el.animate(
     [
       // keyframes
       {
-        borderTopColor: "transparent",
-        borderRightColor: "transparent",
-        borderBottomColor: "white",
-        borderLeftColor: "transparent",
+        borderTopColor: trans,
+        borderRightColor: trans,
+        borderBottomColor: white,
+        borderLeftColor: trans,
       },
       {
-        borderTopColor: "transparent",
-        borderBottomColor: "transparent",
-        borderLeftColor: "transparent",
-        borderRightColor: "white",
+        borderTopColor: trans,
+        borderBottomColor: trans,
+        borderLeftColor: trans,
+        borderRightColor: white,
       },
       {
-        borderBottomColor: "transparent",
-        borderTopColor: "white",
-        borderRightColor: "transparent",
-        borderLeftColor: "transparent",
+        borderBottomColor: trans,
+        borderTopColor: white,
+        borderRightColor: trans,
+        borderLeftColor: trans,
       },
     ],
     {
       // timing options
       duration: 3000,
       iterations: 1,
-      easing: "cubic-bezier(0.12, 0, 0.39, 0)",
+      easing: "linear",
     }
   );
 }
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-5em";
+  }
+  prevScrollpos = currentScrollPos;
+};

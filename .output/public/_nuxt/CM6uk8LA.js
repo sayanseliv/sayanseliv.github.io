89a5d85abd72a8a5ce82bd84313a7340dc3c60cd
Eugen <eugen.lang@zelh.com>
2025-06-21 20:51:32 +0300

    update deploy

---
+++ .output/public/_nuxt/CM6uk8LA.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s}from"./C0vuVvyw.js";const t={name:"Projects"},c={class:"main-projects"};function n(a,r,_,p,i,m){return s(),o("main",c)}const f=e(t,[["render",n]]);export{f as default};

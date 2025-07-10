ef0a378af918fd1b32c1e1e9aff0ced1fb1ed953
Eugen <eugen.lang@zelh.com>
2025-07-10 14:21:41 +0300

    update CircleProgress

--- components/UI/CircleProgress.vue
+++ components/UI/CircleProgress.vue
@@ -15,170 +15,150 @@
 	</div>
 </template>
 
-<script>
+<script lang="ts" setup>
 // https://github.com/w2xi/vue3-circular-progress/tree/main
-
-export default {
-	name: 'CircleProgress',
-	props: {
-		size: {
-			type: Number,
-			default: 180,
-		},
-		borderWidth: {
-			type: Number,
-			default: 15,
-		},
-		borderBgWidth: {
-			type: Number,
-			default: 15,
-		},
-		fillColor: {
-			type: String,
-			default: '#288feb',
-		},
-		contentColor: { type: String, default: '#cacace' },
-		emptyColor: {
-			type: String,
-			default: '#dddddd',
-		},
-		percent: {
-			type: Number,
-			default: 50,
-		},
-		linecap: {
-			type: String,
-			default: 'round',
-		},
-		transition: {
-			type: Number,
-			default: 400,
-		},
-		showPercent: {
-			type: Boolean,
-			default: false,
-		},
+import type { CSSProperties } from 'vue';
+//CircleProgress
+const props = defineProps({
+	size: {
+		type: Number,
+		default: 180,
 	},
-	setup(props) {
-		const circleRadiusBg = () => {
-			const value = (props.size - props.borderBgWidth) * 0.5;
-			if (props.borderWidth > props.borderBgWidth) {
-				value -= (props.borderWidth - props.borderBgWidth) * 0.5;
-			}
-			return value;
-		};
-
-		const circleRadiusFg = () => {
-			const value = (props.size - props.borderWidth) * 0.5;
-			if (props.borderBgWidth > props.borderWidth) {
-				value -= (props.borderBgWidth - props.borderWidth) * 0.5;
-			}
-			return value;
-		};
-
-		const circumference = 2 * Math.PI * circleRadiusFg();
-		const offset = ref(circumference);
-		const currentPercent = ref(0);
-
-		const wrapStyle = {
-			height: props.size + 'px',
-			width: props.size + 'px',
-			position: 'relative',
-		};
-
-		const wrapAttr = {
-			class: props.class,
-			style: wrapStyle,
-		};
-
-		const svgAttr = {
-			version: '1.1',
-			xmlns: 'http://www.w3.org/2000/svg',
-			height: props.size,
-			width: props.size,
-		};
-
-		const cx = props.size / 2;
-		const cy = props.size / 2;
-
-		const circleBgAttr = {
-			cx,
-			cy,
-			r: circleRadiusBg(),
-			fill: 'none',
-			stroke: props.emptyColor,
-			'stroke-width': props.borderBgWidth,
-		};
-
-		const circleFgAttr = computed(() => ({
-			cx,
-			cy,
-			r: circleRadiusFg(),
-			fill: 'none',
-			stroke: props.fillColor,
-			'stroke-width': props.borderWidth,
-			'stroke-dasharray': circumference,
-			'stroke-dashoffset': offset.value,
-			'stroke-linecap': props.linecap,
-			...(props.transition && {
-				style: {
-					transition: `stroke-dashoffset ${props.transition}ms ease`,
-				},
-			}),
-		}));
-
-		function updatePercent() {
-			const percent = Math.round(props.percent);
-
-			animateValue(percent);
-		}
-
-		function animateValue(to) {
-			const step = to - currentPercent.value;
-			const delay = props.transition / Math.abs(step);
-			const counter = setInterval(() => {
-				if (step > 0) {
-					currentPercent.value += 1;
-					if (currentPercent.value >= to) {
-						clearInterval(counter);
-					}
-				} else {
-					currentPercent.value -= 1;
-
-					if (currentPercent.value <= to) {
-						clearInterval(counter);
-					}
-				}
-
-				offset.value = (circumference * (100 - currentPercent.value)) / 100;
-			}, delay);
-		}
-
-		onMounted(() => {
-			const percent = Math.round(props.percent);
-
-			if (percent !== 0) {
-				updatePercent();
-			}
-		});
-
-		watch(
-			() => props.percent,
-			() => {
-				updatePercent();
-			}
-		);
-
-		return {
-			svgAttr,
-			circumference,
-			currentPercent,
-			circleBgAttr,
-			circleFgAttr,
-			wrapAttr,
-		};
+	borderWidth: {
+		type: Number,
+		default: 15,
 	},
+	borderBgWidth: {
+		type: Number,
+		default: 15,
+	},
+	fillColor: {
+		type: String,
+		default: '#288feb',
+	},
+	contentColor: { type: String, default: '#cacace' },
+	emptyColor: {
+		type: String,
+		default: '#dddddd',
+	},
+	percent: {
+		type: Number,
+		default: 50,
+	},
+	linecap: {
+		type: String as () => 'round' | 'inherit' | 'butt' | 'square',
+		default: 'round',
+	},
+	transition: {
+		type: Number,
+		default: 400,
+	},
+	showPercent: {
+		type: Boolean,
+		default: false,
+	},
+	class: {
+		type: String,
+		default: '',
+	},
+});
+const cx = props.size / 2;
+const cy = props.size / 2;
+
+const circleRadiusBg = (): number =>
+	(props.size - props.borderBgWidth) * 0.5 -
+	Math.max(props.borderWidth - props.borderBgWidth, 0) * 0.5;
+
+const circleRadiusFg = (): number =>
+	(props.size - props.borderWidth) * 0.5 -
+	Math.max(props.borderBgWidth - props.borderWidth, 0) * 0.5;
+
+const circumference = 2 * Math.PI * circleRadiusFg();
+const offset = ref(circumference);
+const currentPercent = ref(0);
+
+const wrapStyle: CSSProperties = {
+	height: `${props.size}px`,
+	width: `${props.size}px`,
+	position: 'relative',
 };
+
+const wrapAttr = {
+	class: props.class,
+	style: wrapStyle,
+};
+
+const svgAttr = {
+	version: '1.1',
+	xmlns: 'http://www.w3.org/2000/svg',
+	height: props.size,
+	width: props.size,
+};
+
+const circleBgAttr = {
+	cx,
+	cy,
+	r: circleRadiusBg(),
+	fill: 'none',
+	stroke: props.emptyColor,
+	'stroke-width': props.borderBgWidth,
+};
+
+const circleFgAttr = computed(() => ({
+	cx,
+	cy,
+	r: circleRadiusFg(),
+	fill: 'none',
+	stroke: props.fillColor,
+	'stroke-width': props.borderWidth,
+	'stroke-dasharray': circumference,
+	'stroke-dashoffset': offset.value,
+	'stroke-linecap': props.linecap as 'round' | 'inherit' | 'butt' | 'square',
+	...(props.transition && {
+		style: {
+			transition: `stroke-dashoffset ${props.transition}ms ease`,
+		},
+	}),
+}));
+
+const updatePercent = () => {
+	const percent = Math.round(props.percent ?? 0);
+	animateValue(percent);
+};
+
+function animateValue(to: number) {
+	const step = to - currentPercent.value;
+	const delay = props.transition / Math.abs(step);
+	const counter = setInterval(() => {
+		if (step > 0) {
+			currentPercent.value += 1;
+			if (currentPercent.value >= to) {
+				clearInterval(counter);
+			}
+		} else {
+			currentPercent.value -= 1;
+
+			if (currentPercent.value <= to) {
+				clearInterval(counter);
+			}
+		}
+
+		offset.value = (circumference * (100 - currentPercent.value)) / 100;
+	}, delay);
+}
+
+onMounted(() => {
+	if ((props.percent ?? 0) !== 0) {
+		updatePercent();
+	}
+});
+
+watch(
+	() => props.percent,
+	() => {
+		updatePercent();
+	}
+);
 </script>
 
 <style lang="scss" scoped>

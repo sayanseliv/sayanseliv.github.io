b9299d867428338bf38d16fc56f4e724a827a97d
Eugen <eugen.lang@zelh.com>
2025-07-12 20:03:11 +0300

    add fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -105,6 +105,8 @@ export default defineNuxtConfig({
 				{
 					rel: 'stylesheet',
 					href: 'https://fonts.googleapis.com/css2?family=Barlow&family=Roboto:wght@400;500;700&display=swap',
+					media: 'print',
+					onload: "this.media='all'",
 				},
 				{ rel: 'canonical', href: BASE_URL + '/' },
 				{

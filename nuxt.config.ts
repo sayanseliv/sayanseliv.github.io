d1a863995940c794a4616be5aba0ce10d89896d2
Eugen Lang <sayanseliv@gmail.com>
2026-05-06 01:33:18 +0300

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
 		minify: true,
 		compressPublicAssets: true,
 		prerender: {
-			routes: ['/'],
+			routes: ['/', '/sitemap.xml'],
 			crawlLinks: true,
 		},
 	},
